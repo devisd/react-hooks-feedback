8fa185006fd3c7c81b3a4ca07bcb501fcbcdf7be
devisd <89788799+devisd@users.noreply.github.com>
2022-07-16 00:07:13 +0700

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,16 @@ import Statistics from './Statistics';
 import Notification from './Notification';
 
 import PropTypes from 'prop-types';
+import css from './App.module.css';
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const checkHideOrNot = good + neutral + bad;
+  const total = good + neutral + bad;
+  const percents = Math.round((good / total) * 100) + ' %';
 
   const updateFeedback = e => {
     switch (e.target.textContent) {
@@ -31,77 +36,10 @@ const App = () => {
     }
   };
 
-  // static defaultProps = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  //   total: 0,
-  //   percents: 0,
-  //   message: 'There is no feedback',
-  // };
-
-  // static propTypes = {
-  //   bad: PropTypes.number.isRequired,
-  //   good: PropTypes.number.isRequired,
-  //   neutral: PropTypes.number.isRequired,
-  //   total: PropTypes.number.isRequired,
-  //   percents: PropTypes.number.isRequired,
-  //   message: PropTypes.string.isRequired,
-  //   title: PropTypes.string,
-  // };
-
-  // state = {
-  //   good: this.props.good,
-  //   neutral: this.props.neutral,
-  //   bad: this.props.bad,
-  // };
-
-  // const updateStatistic = e => {
-  //   const value = e.target.textContent;
-  //   return this.setState(prevState => {
-  //     return {
-  //       [value]: prevState[value] + 1,
-  //     };
-  //   });
-  // };
-
-  // function countTotalFeedback() {
-  //   const { good, neutral, bad } = this.state;
-  //   this.total = good + neutral + bad;
-  // }
-
-  // function countPositiveFeedbackPercentage() {
-  //   const { good } = this.state;
-  //   this.percents = Math.round((good / this.total) * 100) + ' %';
-  // }
-
-  // this.countTotalFeedback();
-  // this.countPositiveFeedbackPercentage();
-
-  // const { good, neutral, bad } = this.state;
-
-  const checkHideOrNot = good + neutral + bad;
-  const total = good + neutral + bad;
-  const percents = Math.round((good / total) * 100) + ' %';
-
   return (
-    <div
-      style={{
-        height: 'auto',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        padding: 120,
-      }}
-    >
+    <div className={css.app}>
       <Section title={'Please leave feedback'}>
-        <FeedbackOptions
-          // options={this.state}
-          onLeaveFeedback={updateFeedback}
-        />
+        <FeedbackOptions onLeaveFeedback={updateFeedback} />
       </Section>
 
       <Section title={'Statistics'}>
